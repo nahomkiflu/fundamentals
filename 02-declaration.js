bbadd1cb54e182fd6065b1c79aa90cdd72b7ce2e
nahom kiflu <nahomboy3@gmail.com>
2018-12-04 10:17:37 -0500

    first

---
+++ 02-declaration.js
@@ -0,0 +1,43 @@
+console("hello wolrd")
+
+//cmd ? highlighted : comment out
+/
+ghapha
+
+//console.log is a great a grat
+
+
+
+
+
+
+var b = 1
+var b = 2
+console.log(b) //2
+
+let c = 3
+let c = 4
+console.log(c)
+
+var firstName = "Nahom"
+const lastName = "Kiflu"
+console.log(firstName,lastName)
+console.log (firstName + " " + lastName)
+
+
+
+
+// Declaration are the LEFT SIDE of a VRDisplayCapabilities
+
+
+
+
+
+
+
+
+var x
+console.log()
+
+x = 10
+console.log("initialization i:")
---
+++ 05-type.js
@@ -0,0 +1,107 @@
+// Booleans
+// what is a boolean?
+var on = true;
+console.log(on); // true
+let off = false;
+console.log(off); // false
+
+//booolean can represent; true/false, yes/no, on/off
+
+//null = empty value(not a; not underfined)
+var empty = null;
+console.log(empty); // null
+
+var undef = undefined
+console.log(undef); // underined
+
+let grass;
+console.log(grass); // undefined
+
+/*
+we've talked before about how many variable is basically a storage 
+
+
+
+*/
+
+// numbers
+var degrees = 95;
+console.log(degrees);
+
+var pracise = 9999999999999; // 16 9's
+console.log(pracise); // 999999999999999
+
+var rounded = 9999999999999; // (One more 9)
+console.log(rounded); // 9999999999999
+
+var notQuite = 0.2 + 8.1;
+console.log
+
+var numbersAreHand =(0.2 * 10 + 0.1 * 10) / 10;
+console.log(numbersAreHand); // 0.2
+
+let stringOne = "double quotes";
+
+
+
+// Numbers vs. strings
+let first = 1500 + 100;
+let second = "1050" + "100";
+
+console.log(first);
+console.log(second);
+console.log(typeof first);
+console.let(typeof second);
+
+/*
+Addition vs concatenation
+
+when 35 sees combining two or more numbers, it adds the values together using the built-in
+math functionality; when it sees adding together one or more strings, it changes and the plus
+sigh becomes an assignment operator-- it smashes when the values together without rtying to 
+synthesizes the values
+*/
+
+let third = 1050 + '100';
+
+console.log(third); // 1050100
+console.log(typeof third);
+
+
+
+var firstName = "Nahom"
+var lastName = "kiflu"
+var houseNumber = "1833"
+var street = "somestreet"
+var city = "indianapolis"
+var state = "indiana"
+var zipcode = "46200"
+
+
+//objects 
+/* 
+what is a object?
+a container that can hold multiple datetype
+
+denoted by {}
+Has keys and calues (color (key): 'blue' (value)), separedted with a colon
+each key separated with a comma
+*/
+
+let burritosNow = (
+    size: "large" ,
+    quantity: 4,
+    now: true
+
+)
+console.log(burritosNow);
+
+
+
+
+
+
+
+
+
+
